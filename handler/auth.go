d247fa67001329a737ee6c6f0a38b474948170dd
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-05-24 17:33:50 -0300

    feat: login logic and a bit of htmx usage

--- handler/auth.go
+++ handler/auth.go
@@ -1,10 +1,27 @@
 package handler
 
 import (
+	"dreampicai/pkg/sb"
 	"dreampicai/view/auth"
+	"fmt"
 	"net/http"
+
+	"github.com/nedpals/supabase-go"
 )
 
 func HandleLoginIndex(w http.ResponseWriter, r *http.Request) error {
-	return auth.Login().Render(r.Context(), w)
+	return render(r, w, auth.Login())
+}
+
+func HandleLoginCreate(w http.ResponseWriter, r *http.Request) error {
+	credentials := supabase.UserCredentials{
+		Email:    r.FormValue("email"),
+		Password: r.FormValue("password"),
+	}
+	resp, err := sb.Client.Auth.SignIn()
+	return render(r, w, auth.LoginForm(credentials, auth.LoginErrors{
+		InvalidCredentials: "The credentials you have entered are invalid",
+	}))
+	fmt.Println(credentials)
+	return nil
 }
