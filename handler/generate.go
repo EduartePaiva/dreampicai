41b76a826c1a9b2292ca99a7546ede97c2c66a4b
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-06-01 16:12:17 -0300

    building the image gallery with HTMX completed

--- handler/generate.go
+++ handler/generate.go
@@ -3,7 +3,11 @@ package handler
 import (
 	"dreampicai/types"
 	"dreampicai/view/generate"
+	"fmt"
+	"log/slog"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func HandleGenerateIndex(w http.ResponseWriter, r *http.Request) error {
@@ -11,4 +15,18 @@ func HandleGenerateIndex(w http.ResponseWriter, r *http.Request) error {
 		Images: []types.Image{},
 	}
 	return render(r, w, generate.Index(data))
+}
+
+func HandleGenerateCreate(w http.ResponseWriter, r *http.Request) error {
+	fmt.Println("help")
+	return render(r, w, generate.GalleryImage(types.Image{Status: types.ImageStatusPending}))
+}
+func HandleGenerateImageStatus(w http.ResponseWriter, r *http.Request) error {
+	id := chi.URLParam(r, "id")
+	//fetch from db
+	image := types.Image{
+		Status: types.ImageStatusPending,
+	}
+	slog.Info("checking image status", "id", id)
+	return render(r, w, generate.GalleryImage(image))
 }
